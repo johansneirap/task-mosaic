125d3126785bdfed06c966b504b019cefbb4d30c
Johans Neira <johansneirap@gmail.com>
2023-09-05 00:24:04 -0300

    feat: summarize

---
+++ app/api/generateSummary/route.ts
@@ -0,0 +1,33 @@
+import openai from "@/openai";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { todos } = await request.json();
+  console.log(todos);
+
+  // comunicate with OpenAI GPT
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0.8,
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        role: "system",
+        content:
+          "When responding, welcome the user always as Johans and say Welcome to the Task Mosaic TODO App! Limit the response to 200 characters",
+      },
+      {
+        role: "user",
+        content: `Hi there, provide a summary of the following todos.
+        Count how many todos are in each category such as To do, in progress and done, then tell the user to have a productive day! Here is the data ${JSON.stringify(
+          todos
+        )}`,
+      },
+    ],
+  });
+
+  const { choices } = response;
+
+  return NextResponse.json(choices[0].message);
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,31 @@
 "use client";
 
+import fetchSuggestion from "@/lib/fetchSuggestion";
 import { useBoardStore } from "@/store/BoardStore";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import Avatar from "react-avatar";
 
 function Header() {
-  const [searchString, setSearchString] = useBoardStore((state) => [
+  const [board, searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
     state.searchString,
     state.setSearchString,
   ]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [suggestion, setSuggestion] = useState<string>("");
+  useEffect(() => {
+    if (board.columns.size === 0) return;
+    setLoading(true);
+
+    const fetchSuggestionFunc = async () => {
+      const suggestion = await fetchSuggestion(board);
+      setSuggestion(suggestion);
+      setLoading(false);
+    };
+    fetchSuggestionFunc();
+  }, [board]);
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
@@ -30,6 +46,7 @@ function Header() {
               placeholder="Search"
               className="flex-1 outline-none p-2"
               onChange={(e) => setSearchString(e.target.value)}
+              value={searchString}
             />
             <button type="submit" hidden>
               Search
@@ -40,8 +57,14 @@ function Header() {
       </div>
       <div className="flex items-center justify-center px-5 py-2 md:py-5">
         <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
-          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
-          GPT is summarising your tasks for the day...
+          <UserCircleIcon
+            className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${
+              loading && "animate-spin"
+            }`}
+          />
+          {suggestion && !loading
+            ? suggestion
+            : "GPT is summarising your tasks for the day..."}
         </p>
       </div>
     </header>
